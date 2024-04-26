288af651d60d872165c1b957da757ad36c896ce0
fyng <feiyangthegoat@gmail.com>
2024-04-25 21:24:31 -0400

    implement Q learning, add gym template

---
+++ gym.py
@@ -0,0 +1,16 @@
+# https://gymnasium.farama.org/content/basic_usage/
+import gymnasium as gym
+
+
+
+# Acrobat
+# https://gymnasium.farama.org/environments/classic_control/acrobot/
+
+
+
+
+
+# Mountain Car
+# https://gymnasium.farama.org/environments/classic_control/mountain_car/
+env = gym.make('MountainCar-v0')
+observation = env.reset()
--- qlearning.py
+++ qlearning.py
@@ -1,22 +1,59 @@
 from evaluation import *
 import matplotlib.pyplot as plt
 from maze import *
+from tqdm import tqdm
 
-# TODO: intialize
-lr = 0.1 # update to Q table
-epsilon = 0.1 # chance of taking a random action
+def rmse(predictions, targets):
+    return np.sqrt(np.mean((predictions-targets)**2))
 
-eval_step, eval_reward = [] , []
-learning = True
-while learning:
+
+lr = 0.6 
+epsilon = 0.05
+max_iter = 5000
+discount = 0.9
+eval_step = []
+eval_reward = []
+eval_error = []
+
+env = Maze()
+env.reset()
+
+Q_table = np.zeros((env.snum, env.anum))
+Q_optim = np.load('Qval.npy')
+
+for i in tqdm(range(max_iter)):  
     # TODO: implement Q learning
 
-    avg_step, avg_reward = evaluation(Maze(), current_Q_table)
-    eval_step.append(avg_step)
-    eval_reward.append(avg_reward)
+    for state in range(env.snum):
+        for action in range(env.anum):
+            reward, next_state, done = env.step(state, action)
 
-f1, ax1 = plt.subplots()
-ax1.plot(np.arange(0, 5000, 50), eval_step)
+            # epsilon greedy
+            if np.random.rand() < epsilon:
+                next_action = np.random.randint(env.anum)
+            else:
+                next_action = np.argmax(Q_table[next_state])
 
-f2, ax2 = plt.subplots()
-ax2.plot(np.arange(0, 5000, 50), eval_reward)
+            # update Q table
+            Q_table[state, action] = (1 - lr) * Q_table[state, action] + lr * (reward + discount * np.max(Q_table[next_state, next_action]))
+
+    if (i+1) % 50 == 0:
+        avg_step, avg_reward = evaluation(Maze(), Q_table)
+        eval_step.append(avg_step)
+        eval_reward.append(avg_reward)
+    error = rmse(Q_table, Q_optim)
+    eval_error.append(error)
+
+print(len(eval_step), len(eval_reward), len(eval_error))
+
+f1, axes = plt.subplots(1, 3, figsize=(12, 5))
+axes[0].plot(range(0, max_iter, 50), eval_step)
+axes[0].set_title('Average steps to goal in an episode')
+
+axes[1].plot(range(0, max_iter, 50), eval_reward)
+axes[1].set_title('Average reward in an episode')
+
+axes[2].plot(eval_error)
+axes[2].set_title('RMSE between Q values')
+plt.tight_layout()
+plt.savefig(f'plots/Q_lr{lr}_ep{epsilon}.png')
